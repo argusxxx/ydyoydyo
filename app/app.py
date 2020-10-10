141397dd2bbaa2c3deee4301ee5b03c99320b7ee
argusxxx <72632403+argusxxx@users.noreply.github.com>
2020-10-10 09:31:33 +0300

    Add files via upload

---
+++ app/app.py
@@ -0,0 +1,95 @@
+import os
+from flask import Flask, render_template, request, send_file, redirect, url_for
+from werkzeug.utils import secure_filename
+import pandas as pd
+import csv
+from io import StringIO 
+from datetime import datetime
+
+from app.attendance import calculate_attendance
+
+UPLOAD_FOLDER = './app/uploads/'
+
+app = Flask(__name__, template_folder='templates')
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    return render_template('index.html')
+
+
+@app.route("/data", methods=["GET", "POST"])
+def data():
+    
+    if request.method == "POST":
+        #Time Format for Chrome
+        time_format = '%H:%M'
+
+        #Convert time from html to python datetime.
+        # end_time = datetime.strptime(request.form['end_time'], time_format)
+        start_time = request.form['start_time']
+        end_time = request.form['end_time']
+
+        #Get file uploaded by user
+        file = request.files['attendance-file']
+        filename = secure_filename(file.filename)
+        print(f'FILENAME: {filename}')
+        #get file extension
+        file_extension = os.path.splitext(filename)[1]
+        filename_without_extension = os.path.splitext(filename)[0]
+
+        #save file to uploads folder
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        #read file as pandas dataframe considering file extension
+        if file_extension == '.csv':
+            #For some users, Microsoft Teams returns a .csv file with UTF-8 encoding while for others UTF-16 encoding
+            #Try UTF-8 otherwise UTF-16 encoding with "tab" delimiter
+            try:
+                df = pd.read_csv(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            except:
+                df = pd.read_csv(os.path.join(app.config['UPLOAD_FOLDER'], filename), sep='\t', encoding='utf-16')
+        elif file_extension == '.xlsx' or file_extension == '.xls':
+            df = pd.read_excel(os.path.join(app.config['UPLOAD_FOLDER'], filename)) 
+        else:
+            return "Unsupported file type please upload .csv, .xlsx or .xls file"
+        print(df)
+        
+        #calculate time attended and return a dataframe
+        result = calculate_attendance(df, start_time, end_time)
+        print(result)
+        result_copy = result
+        #convert result dataframe to .csv file for user to download
+        result_filename = "[ATTENDANCE]" + filename_without_extension + ".csv"
+        result.to_csv(UPLOAD_FOLDER + result_filename, index=False)
+
+        #Delete file from storage after creating dataframe
+        # os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        #send file name as parameter to downlad
+        # return redirect(url_for('download_file', filename=filename, result=result_copy))
+        return render_template('download.html', value=result_filename, result=result.to_html())
+
+    return render_template('data.html')
+
+
+#Download demo file 'test.csv'
+@app.route('/demo.csv')
+def download_demo_file():
+    demo_file_path = 'uploads/test.csv'
+    return send_file(demo_file_path, as_attachment=False, attachment_filename='test.csv')
+
+# @app.route('/view-file')
+# def view_file():
+
+#     print(f'DATAFRAME: {df}')
+#     return render_template('index.html', original_file=df)
+
+@app.route('/return-files/<filename>')
+def return_files_tut(filename):
+    file_path = 'uploads/' + filename
+    return send_file(file_path, as_attachment=True, attachment_filename='')
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
---
+++ app/attendance.py
@@ -0,0 +1,63 @@
+import pandas as pd
+from datetime import datetime
+
+def calculate_attendance(df, start_time, end_time):
+    #Remove Duplicates
+    students_present = df['Full Name']
+    students_present = list(dict.fromkeys(students_present))
+
+    df2 = pd.DataFrame(columns = ['Name', 'Time Attended']) 
+    df2['Name'] = students_present
+
+    student_attendance = []
+
+    # get meeting start time from first row in attendance sheet
+    meeting_start_time = df['Timestamp'][0]
+
+    # get meeting date by separating date and time into lists
+    time_split = meeting_start_time.split(',')
+    meeting_date = time_split[0]
+
+    #Every browser has a different time format
+    browser_time_format = '%H:%M'
+    
+    #Find time format used in sheet
+    try:
+        time_format = '%d/%m/%Y, %H:%M:%S'
+        time_test = datetime.strptime(meeting_start_time, time_format)
+        #Convert time returned from browser into format similar to MS Teams attendance sheet
+        time1 = datetime.strptime(start_time, browser_time_format).strftime('%H:%M:%S')
+        time2 = datetime.strptime(end_time, browser_time_format).strftime('%H:%M:%S')
+    except:
+        time_format = '%m/%d/%Y, %I:%M:%S %p'
+        time_test = datetime.strptime(meeting_start_time, time_format)
+        #Convert time returned from browser into format similar to MS Teams attendance sheet
+        time1 = datetime.strptime(start_time, browser_time_format).strftime('%I:%M:%S %p')
+        time2 = datetime.strptime(end_time, browser_time_format).strftime('%I:%M:%S %p')
+
+    meeting_start_time = meeting_date + ", " + time1
+    meeting_end_time = meeting_date + ", " + time2
+    print(f' END TIME:{end_time}')
+    print(f'MEETING END TIME:{meeting_end_time}')
+    meeting_start_time = datetime.strptime(meeting_start_time, time_format)
+    meeting_end_time = datetime.strptime(meeting_end_time, time_format)
+    time_attended = meeting_end_time - meeting_start_time
+
+    for i in df2.index:
+        student_join_time = meeting_start_time
+        student_leave_time = meeting_end_time
+        time_attended = meeting_end_time - meeting_end_time
+        for j in df.index:
+            if df2['Name'][i] == df['Full Name'][j]:
+                if df['User Action'][j] == "Joined before":
+                    student_join_time = meeting_start_time
+                    time_attended = meeting_end_time - student_join_time
+                if df['User Action'][j] == 'Left':
+                    student_leave_time = datetime.strptime(df['Timestamp'][j], time_format)
+                    time_attended = time_attended - (meeting_end_time - student_leave_time)
+                if df['User Action'][j] == "Joined":
+                    student_join_time = datetime.strptime(df['Timestamp'][j], time_format)
+                    time_attended = time_attended + (meeting_end_time - student_join_time)
+            df2['Time Attended'][i] = time_attended
+
+    return df2
